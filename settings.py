f8a0edb782d4251c364722d99d7063d01cd65688
faraway_c <chongyanyi@126.com>
2023-06-08 17:57:38 +0800

    add code of score

--- settings.py
+++ settings.py
@@ -13,14 +13,43 @@ class Settings:
         self.ship_limit = 1
 
         # 子弹设置
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
         # 外星人设置
-        self.alien_speed = 0.5
+        self.alien_speed = 0.3
         self.fleet_drop_speed = 10
         # fleet_direction为1表示向右移动，为-1表示向左移动
         self.fleet_direction = 1
+
+        # 计分
+        self.alien_points = 50
+
+        # 加快游戏节奏的速度
+        self.speedup_scale = 1.2
+
+        # 外星人分数提高的速度
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """初始化随游戏进行而变化的设置"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 1.5
+        self.alien_speed = 0.3
+        self.alien_points = 50
+
+        # fleet_direction为1表示向右，为-1表示向左
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """提高速度设置"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
